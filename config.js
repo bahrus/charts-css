efe1866390d3bd0f01e41685784359950de05bb4
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-02-13 20:02:12 -0500

    Update README.md

---
+++ config.js
@@ -0,0 +1,10 @@
+// @ts-ref
+import {MntCfgMxn} from 'trans-render/MntCfgMxn.js';
+/** @import {AllProps, Actions} from  './ts-refs/css-charts/types' */
+/** @import {MntCfg, MountProps, MountActions} from './ts-refs/trans-render/types' */
+
+/**
+ * @type {MntCfg<AllProps & MountProps, Actions & MountActions>}
+ */
+export const config = {
+}
---
+++ css-charts.js
@@ -0,0 +1,13 @@
+// @ts-check
+import {Mount} from 'trans-render/Mount.js';
+
+/** @import {AllProps, Actions} from  './ts-refs/css-charts/types' */
+/** @import {MntCfg, MountProps, MountActions} from './ts-refs/trans-render/types' */
+
+
+/**
+ * @implements {Actions}
+ */
+export class CSSCharts extends Mount {
+
+}
